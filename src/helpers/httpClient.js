02d868b58e5b8c822fac2b1285a83af013352731
ASUBRIX <asubrix@gmail.com>
2025-05-21 16:03:41 +0530

    feat:add backend url

--- src/helpers/httpClient.js
+++ src/helpers/httpClient.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 function HttpClient() {
   const instance = axios.create({
-    baseURL:import.meta.env.VITE_API_BASE_URL,
+    baseURL:'https://server.pudhuyugamacademy.com',
     timeout: 15000,
   });
 
