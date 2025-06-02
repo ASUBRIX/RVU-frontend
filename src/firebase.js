1806f16a841d9910031cbe9dc80d131807af02f9
ASUBRIX <asubrix@gmail.com>
2025-06-02 06:25:43 +0530

    feat:Sign up using firebase

--- src/firebase.js
+++ src/firebase.js
@@ -12,15 +12,6 @@ const firebaseConfig = {
   appId: "1:424669739291:web:87c81cc19c9cd4cfa53bbe"
 };
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyB55cNCVlXLi42CMegRLloTM9Yy5CRZTYY",
-//   authDomain: "pudhuyugam-7fb6c.firebaseapp.com",
-//   projectId: "pudhuyugam-7fb6c",
-//   storageBucket: "pudhuyugam-7fb6c.firebasestorage.app",
-//   messagingSenderId: "424669739291",
-//   appId: "1:424669739291:web:87c81cc19c9cd4cfa53bbe"
-// };
-
 
 const app = initializeApp(firebaseConfig);
 
