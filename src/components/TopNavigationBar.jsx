0f9604fc78cca41cc2311ebc2539921520d00405
ASUBRIX <asubrix@gmail.com>
2025-06-03 17:27:03 +0530

    feat:keep margin on logo

--- src/components/TopNavigationBar.jsx
+++ src/components/TopNavigationBar.jsx
@@ -34,7 +34,7 @@ const TopNavigationBar = () => {
       <nav className="navbar navbar-expand-xl z-index-2 py-0">
         <Container fluid className="px-2 px-xl-4">
           {/* Logo left */}
-          <div className="d-flex align-items-center" style={{ marginLeft: 6 }}>
+          <div className="d-flex align-items-center" style={{ marginLeft: 28 }} >
             <LogoBox height={90} width={240} />
           </div>
           {/* Toggler for mobile */}
