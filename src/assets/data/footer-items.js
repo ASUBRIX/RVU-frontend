a2ebfd299dbef932bd6cff2eb4abc32b9faead53
ASUBRIX <asubrix@gmail.com>
2025-06-07 13:28:14 +0530

    feat:Footer

--- src/assets/data/footer-items.js
+++ src/assets/data/footer-items.js
@@ -1,113 +1,42 @@
 import { FaFacebookF, FaInstagram, FaLinkedinIn, FaTwitter } from 'react-icons/fa'
+
 export const footerLinks = [
   {
-    title: 'Quick links',
+    title: 'Quick Links',
     items: [
-      {
-        name: 'Home',
-        link: '/',
-      },
-      {
-        name: 'About',
-        link: '/about-us',
-      },
-      {
-        name: 'Our Team',
-        link: '/our-team',
-      },
-      {
-        name: 'Courses',
-        link: '/courses',
-      },
-      {
-        name: 'Free Test',
-        link: '/free-test',
-      },
-      {
-        name: 'Gallery',
-        link: '/gallery',
-      },
-      {
-        name: 'Blogs',
-        link: '/blogs',
-      },
+      { name: 'Home', link: '/' },
+      { name: 'About', link: '/about-us' },
+      { name: 'Our Team', link: '/our-team' },
+      { name: 'Courses', link: '/courses' },
+      { name: 'Free Test', link: '/free-test' },
+      { name: 'Gallery', link: '/gallery' },
+      { name: 'Blogs', link: '/blogs' },
+    ],
+  },
+  {
+    title: 'Quick Links',
+    items: [
+      { name: 'Support', link: '/contact-us' },
+      { name: 'FAQ', link: '/faq' },
+      { name: 'Terms and Conditions', link: '/terms-conditions' },
+      { name: 'Privacy Policy', link: '/privacy-policy' },
     ],
   },
   {
     title: 'Courses',
     items: [
-      {
-        name: 'Test Series',
-        link: '/pages/course/grid-2',
-      },
-      {
-        name: 'TNPSC',
-        link: '/pages/course/grid-2',
-      },
-      {
-        name: 'UPSC CSE',
-        link: '/pages/course/grid-2',
-      },
-      {
-        name: 'Bank Exam',
-        link: '/pages/course/grid-2',
-      },
-      {
-        name: 'Railways Exam',
-        link: '/pages/course/grid-2',
-      },
+      { name: 'Test Series', link: '/pages/course/grid-2' },
+      { name: 'TNPSC', link: '/pages/course/grid-2' },
+      { name: 'UPSC CSE', link: '/pages/course/grid-2' },
+      { name: 'Bank Exam', link: '/pages/course/grid-2' },
+      { name: 'Railways Exam', link: '/pages/course/grid-2' },
     ],
   },
-  {
-    title: 'Support',
-    items: [
-      {
-        name: 'Contact',
-        link: '/contact-us',
-      },
-      {
-        name: 'FAQ',
-        link: '/faq',
-      },
-    ],
-  },
-]
-export const footerLinks2 = [
-  {
-    name: 'About',
-    link: '/about-us',
-  },
-  {
-    name: 'Terms',
-  },
-  {
-    name: 'Privacy',
-  },
-  {
-    name: 'Career',
-  },
-  {
-    name: 'Contact us',
-  },
-  {
-    name: 'Cookies',
-  },
-]
+];
+
 export const socialMediaLinks = [
-  {
-    icon: FaFacebookF,
-    variant: 'text-facebook',
-  },
-  {
-    icon: FaInstagram,
-    variant: 'text-instagram',
-  },
-  {
-    icon: FaTwitter,
-    variant: 'text-twitter',
-  },
-  {
-    icon: FaLinkedinIn,
-    variant: 'text-linkedin',
-  },
-]
+  { icon: FaFacebookF, variant: 'text-facebook' },
+  { icon: FaInstagram, variant: 'text-instagram' },
+  { icon: FaTwitter, variant: 'text-twitter' },
+  { icon: FaLinkedinIn, variant: 'text-linkedin' },
+];
