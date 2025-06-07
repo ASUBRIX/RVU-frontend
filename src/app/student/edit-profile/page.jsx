f5d0906ba8cbf0d0968edaf2b7c7f53a37f18081
ASUBRIX <asubrix@gmail.com>
2025-06-07 07:50:11 +0530

    Fix:remove change password & email

--- src/app/student/edit-profile/page.jsx
+++ src/app/student/edit-profile/page.jsx
@@ -1,33 +1,20 @@
-import PageMetaData from '@/components/PageMetaData';
-import { Container, Row, Col } from 'react-bootstrap';
-import EditProfile from './components/EditProfile';
-import EmailChange from './components/EmailChange';
-import PasswordChange from './components/PasswordChange';
+import PageMetaData from '@/components/PageMetaData'
+import { Container, Row, Col } from 'react-bootstrap'
+import EditProfile from './components/EditProfile'
 
 const EditProfilePage = () => (
   <>
     <PageMetaData title="Edit Profile" />
-    <section className="py-4" style={{ background: "#f8fafc", minHeight: "100vh" }}>
+    <section className="py-4" style={{ background: '#f8fafc', minHeight: '100vh' }}>
       <Container>
         <Row className="justify-content-center mb-4">
           <Col lg={8} md={10}>
             <EditProfile />
-            <div className="mt-4">
-              <EmailChange />
-            </div>
           </Col>
         </Row>
-        
-        <Container>
-          <Row className="justify-content-center">
-            <Col lg={6} md={8}>
-              <PasswordChange />
-            </Col>
-          </Row>
-        </Container>
       </Container>
     </section>
   </>
-);
+)
 
-export default EditProfilePage;
+export default EditProfilePage
--- src/helpers/studentApi.js
+++ src/helpers/studentApi.js
@@ -11,16 +11,3 @@ export const updateProfile = async (profileData) => {
   const res = await httpClient.put('/api/student/profile', profileData);
   return res.data;
 };
-
-
-// Change email
-export const changeEmail = async (email) => {
-  const res = await httpClient.put('/api/student/profile', { email });
-  return res.data;
-};
-
-// Change password
-export const changePassword = async (payload) => {
-  const res = await httpClient.put('/api/student/profile', payload);
-  return res.data;
-};
