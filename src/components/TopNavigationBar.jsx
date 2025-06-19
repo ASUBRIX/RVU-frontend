eb8b2d9ad3d8005410e66c117a49ee9a5efd53dd
ASUBRIX <asubrix@gmail.com>
2025-06-19 11:52:56 +0530

    Fix:Style

--- src/components/TopNavigationBar.jsx
+++ src/components/TopNavigationBar.jsx
@@ -18,7 +18,7 @@ const TopNavigationBar = () => {
     { label: 'About', path: '/about-us' },
     { label: 'Our Team', path: '/our-team' },
     { label: 'Courses', path: '/courses' },
-    { label: 'Test', path: '/free-test' },
+    { label: 'Free Test', path: '/free-test' },
     { label: 'Gallery', path: '/gallery' },
     { label: 'Blogs', path: '/blogs' },
     { label: 'Current Affairs', path: '/current-affairs' },
