01e6a2a531db7b395f2dcb5929f15ef17c0a9c04
ASUBRIX <asubrix@gmail.com>
2025-06-25 15:41:57 +0530

    Fix:bug

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import {getAuth} from "firebase/auth"
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB55cNCVlXLi42CMegRLloTM9Yy5CRZTYY",
-  authDomain: "pudhuyugam-7fb6c.firebaseapp.com",
-  projectId: "pudhuyugam-7fb6c",
-  storageBucket: "pudhuyugam-7fb6c.appspot.com",
-  messagingSenderId: "424669739291",
-  appId: "1:424669739291:web:87c81cc19c9cd4cfa53bbe"
+  apiKey: "AIzaSyDusKxKQ3b9GgrbUW8tbE8qxwoBY5vjpAk",
+  authDomain: "royal-victorian-universi-fd4b0.firebaseapp.com",
+  projectId: "royal-victorian-universi-fd4b0",
+  storageBucket: "royal-victorian-universi-fd4b0.firebasestorage.app",
+  messagingSenderId: "744287874838",
+  appId: "1:744287874838:web:82385c65cef9def8262a37"
 };
 
 
